a0a8c86ce26b46807374c926c2d3733d561fb47e
Daniel West <daniel.j.west@gmail.com>
2016-04-19 20:39:53 +0000

    Fixing silly auth expiration problem

--- api/middleware/authMiddleware.js
+++ api/middleware/authMiddleware.js
@@ -10,7 +10,7 @@ export default (router) => {
       // verifies secret and checks exp
       jwt.verify(token, config.api_secret, function (err, decoded) {
         if (err) {
-          return res.json({ success: false, message: 'Invalid token' })
+          return res.status(401).json({ success: false, message: 'Invalid token' })
         } else {
           // if everything is good, save to request for use in other routes
           req.decoded = decoded
@@ -20,7 +20,7 @@ export default (router) => {
     } else {
       // if there is no token
       // return an error
-      return res.status(403).send({
+      return res.status(401).send({
         success: false,
         message: 'No token provided.'
       })
